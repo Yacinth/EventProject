cc1364cfe9903b4aeb6005f089982e1005aefa13
Yacinth <fevriercjjc@gmail.com>
2019-02-11 15:28:22 +0100

    3 models create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,2 @@
 class User < ApplicationRecord
-   after_create :welcome_send
-
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
-
 end
--- db/migrate/20190211141026_create_users.rb
+++ db/migrate/20190211141026_create_users.rb
@@ -1,8 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
       t.string :email
+      t.string :encrypted_password
+      t.text :description
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
