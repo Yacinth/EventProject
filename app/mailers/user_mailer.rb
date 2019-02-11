7254f340e6c2155eb38abe745038428987647e03
Yacinth <fevriercjjc@gmail.com>
2019-02-12 00:36:15 +0100

    all rspec

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,14 +12,14 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
 
-  def alert_to_admin_send(user)
+  def alert_to_admin_send(admin_event)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-    @user = user 
+    @admin_event = admin_event 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'https://event-project.herokuapp.com/' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Une personne s est inscrite sur ton event !') 
+    mail(to: @admin_event.email, subject: 'Une personne s est inscrite sur ton event !') 
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -19,26 +19,40 @@ context "validation" do
 
   describe "#start_date" do
     it { expect(@event).to validate_presence_of(:start_date) }
-  end
-  
-  it "date_start cannot be in the past" do
-   expect(@event.start_date).to be >Time.now
+    
+    it "date_start cannot be in the past" do
+    expect(@event.start_date).to be >Time.now
+    end
   end
 
   describe "#duration" do
     it { expect(@event).to validate_presence_of(:duration) }
+    
+    it { expect(@event.duration%5).to eq(0) }
   end
 
   describe "#title" do
     it { expect(@event).to validate_presence_of(:title) }
+
+    it "title length" do
+    expect(@event.title.length).to be >5 && be <140
+    end
   end
 
   describe "#description" do
     it { expect(@event).to validate_presence_of(:description) }
+
+    it "description length" do
+    expect(@event.description.length).to be >20 && be <1000
+    end
   end
 
   describe "#price" do
     it { expect(@event).to validate_presence_of(:price) }
+
+    it "value of price must be between 1 and 1000 " do
+    expect(@event.description.length).to be >1 && be <1000
+    end
   end
 
   describe "#title" do
@@ -51,11 +65,11 @@ end
 
 context "associations" do
 
-  describe "events" do
+  describe "events_associate_to_participants" do
     it { expect(@event).to have_many(:participants) }
   end
 
-  describe "events" do
+  describe "events_associate_to_attendances" do
     it { expect(@event).to have_many(:attendances) }
   end
 
