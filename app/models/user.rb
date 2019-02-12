ee1181afbb494f39ca9c67ab799baa9bc621aed8
Yacinth <fevriercjjc@gmail.com>
2019-02-12 18:12:33 +0100

    comm ligne timestamp

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   before_save {  |user| user.email = email.downcase }
   after_create :welcome_send
   
